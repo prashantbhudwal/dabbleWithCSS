5fb1f27bb700d01715429f5cea5c64f5b6ce2162
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-01-31 19:38:58 +0530

    Render options with Js

--- components/dropdown/index.js
+++ components/dropdown/index.js
@@ -3,7 +3,30 @@ const optionsContainer = document.getElementById("optionsContainer");
 const optionsContainerClasses =
   document.getElementById("optionsContainer").classList;
 
-placeholder.addEventListener("click", () => {
+const optionsObject = {
+  1: "Dog",
+  2: "Cat",
+  3: "Hamster",
+};
+
+const getOptionsHTML = function (option) {
+  return `<div class="option" ?>${optionsObject[option]}</div>
+    `;
+};
+
+const renderOptions = function () {
+  optionsContainer.innerHTML = "";
+  for (let option in optionsObject) {
+    optionsContainer.innerHTML += getOptionsHTML(option);
+  }
+};
+
+const toggleOptions = function () {
   optionsContainer.style.display =
     optionsContainer.style.display === "none" ? "block" : "none";
+};
+
+placeholder.addEventListener("click", () => {
+  renderOptions();
+  toggleOptions();
 });
